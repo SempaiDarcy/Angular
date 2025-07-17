d5a63e82c669f58b9b58b3936e3883b3d40fda00
SempaiDarcy <mr_darcy_1@mail.ru>
2025-07-17 12:47:17 +0500

    feat: create calculator component with input binding and calculation logic [calculator]

---
+++ src/app/my-calculator/my-calculator.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-calculator',
+  templateUrl: './my-calculator.component.html',
+  styleUrls: ['./my-calculator.component.scss']
+})
+export class MyCalculatorComponent {
+
+  public first: number = 1;
+  public second: number = 1;
+  public operation: string = "+";
+
+  public operations: string[]  = [
+    "+",
+    "-",
+    "*",
+    "/",
+  ]
+
+  // public result: number | undefined = undefined;
+  public result?: number;
+
+  public calc () {
+    switch (this.operation) {
+      case '+':
+        this.result = this.first + this.second;
+        break;
+      case '-':
+        this.result = this.first - this.second;
+        break;
+      case '*':
+        this.result = this.first * this.second;
+        break;
+      case '/':
+        this.result = this.first / this.second;
+        break;
+    }
+  }
+
+}
